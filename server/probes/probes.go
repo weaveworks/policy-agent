8d01c2ea206a8c4d1d55edcf0a5aa3659ff5d25c
alichaddad <ali.chaddad@magalix.com>
2022-01-26 11:33:37 +0200

    add probes server

---
+++ server/probes/probes.go
@@ -0,0 +1,61 @@
+package probes
+
+import (
+	"context"
+	"net/http"
+	"sync/atomic"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type ProbesHandler struct {
+	address string
+	ready   *atomic.Value
+}
+
+func NewProbesHandler(address string) *ProbesHandler {
+	ready := &atomic.Value{}
+	ready.Store(false)
+	return &ProbesHandler{
+		address: address,
+		ready:   ready,
+	}
+}
+
+func (p *ProbesHandler) healthHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader((http.StatusOK))
+}
+
+func (p *ProbesHandler) isReady() bool {
+	if !p.ready.Load().(bool) {
+		return false
+	}
+	return true
+}
+
+func (p *ProbesHandler) readinessHandler(w http.ResponseWriter, _ *http.Request) {
+	if p.isReady() {
+		w.WriteHeader(200)
+	} else {
+		w.WriteHeader(503)
+	}
+}
+
+func (p *ProbesHandler) SetReady() {
+	p.ready.Store(true)
+}
+
+func (p *ProbesHandler) Run(ctx context.Context) error {
+	eg, _ := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		mux := http.NewServeMux()
+		mux.Handle("/health", http.HandlerFunc(p.healthHandler))
+		mux.Handle("/ready", http.HandlerFunc(p.readinessHandler))
+		server := &http.Server{
+			Addr:    p.address,
+			Handler: mux,
+		}
+		return server.ListenAndServe()
+	})
+	return eg.Wait()
+}
