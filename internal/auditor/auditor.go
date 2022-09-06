29789948120bcc2086cb605db6b59679fe61c646
Waleed Hammam <waleed.hamam@weave.works>
2022-09-06 14:52:14 +0200

    change error message

--- internal/auditor/auditor.go
+++ internal/auditor/auditor.go
@@ -21,7 +21,7 @@ type AuditorController struct {
 // NewAuditController returns a new instance of AuditController with an audit event listener
 func NewAuditController(validator validation.Validator, auditInterval time.Duration, entitiesSources ...domain.EntitiesSource) *AuditorController {
 	if auditInterval < 1 {
-		logger.Fatal("audit interval must positive number, current interval: ", auditInterval)
+		logger.Fatal("audit interval can not be less than 1 hour, current interval: ", auditInterval)
 	}
 	auditController := &AuditorController{
 		entitiesSources: entitiesSources,
